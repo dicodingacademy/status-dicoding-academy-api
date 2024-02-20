9e63853a4a515d8460ee3eb246c08b2f0d88bac2
Nur Rizki Adi Prasetyo <nrizki@dicoding.com>
2024-02-20 09:07:45 +0700

    Finish development showing status api

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
+import '@/style.css';
+
 import { createApp } from 'vue';
+import App from '@/App.vue';
 
-import './style.css';
-import App from './App.vue';
-
-createApp(App).mount('#app');
+const app = createApp(App);
+app.mount('#app');
